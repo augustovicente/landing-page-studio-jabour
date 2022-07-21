8371fc9713d28a2f542d9da2b83cde7010f64bf2
Augusto Vicente <devguto@gmail.com>
2022-07-21 20:55:23 -0300

    progress init

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect } from 'react'
-import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
     // changin the title of the page
@@ -10,8 +9,23 @@ const Home: NextPage = () => {
         document.title = 'Studio Jabour'
     }, [])
     return (
-        <div className={styles.container}>
-            
+        <div className='page'>
+            <div className='section-1'>
+                <div className='header'>
+                    <div className='logos'>
+                        
+                    </div>
+                    <div className='links'>
+                        <a href="">Home</a>
+                        <a href="">Serviços</a>
+                        <a href="">Promoções</a>
+                        <a href="">Contato</a>
+                    </div>
+                </div>
+                <div className='content'>
+                    
+                </div>
+            </div>
         </div>
     )
 }
